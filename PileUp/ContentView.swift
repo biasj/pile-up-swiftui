4cc58f62336dfbd0a5c70b62fb8ca2b22bc61668
Beatriz Sato <50559620+biasj@users.noreply.github.com>
2021-04-04 18:19:52 -0300

    Initial Commit

---
+++ PileUp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PileUp
+//
+//  Created by Beatriz Sato on 04/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDisplayName("iPad Pro")
+            .previewLayout(.fixed(width: 1366, height: 1024))
+    }
+}
---
+++ PileUp/PileUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PileUpApp.swift
+//  PileUp
+//
+//  Created by Beatriz Sato on 04/04/21.
+//
+
+import SwiftUI
+
+@main
+struct PileUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
