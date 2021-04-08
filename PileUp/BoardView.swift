d979069d36413a3f32913292bc42fbc86f527742
Beatriz Sato <50559620+biasj@users.noreply.github.com>
2021-04-07 23:14:42 -0300

    Adiciona fundo do grid

--- PileUp/BoardView.swift
+++ PileUp/BoardView.swift
@@ -14,23 +14,33 @@ struct BoardView: View {
     private let columns: [GridItem] = Array(repeating: .init(.fixed(160), spacing: 10), count: 4)
     
     var body: some View {
-        LazyVGrid(columns: columns, spacing: 0) {
-            ForEach(0..<puzzle.board.blocks.count) { i in
-                ZStack {
-                    Image("shadow").resizable().frame(width: 150, height: 75, alignment: .center)
-                    
-                    // check if any GoalView should be placed at the current index
-                    if puzzle.board.shouldPlaceGoal(index: i) {
-                        GoalView(goal: puzzle.board.goals[puzzle.board.getGoalIndex(of: i)])
+        ZStack {
+            // grid background
+            RoundedRectangle(cornerRadius: 10).frame(width: 700, height: 450, alignment: .center).foregroundColor(.gray)
+            
+            
+            // board grid 4x4
+            LazyVGrid(columns: columns, spacing: 0) {
+                ForEach(0..<puzzle.board.blocks.count) { i in
+                    ZStack {
+                        Image("shadow").resizable().frame(width: 150, height: 75, alignment: .center)
+                        
+                        // check if any GoalView should be placed at the current index
+                        if puzzle.board.shouldPlaceGoal(index: i) {
+                            GoalView(goal: puzzle.board.goals[puzzle.board.getGoalIndex(of: i)])
+                        }
+                        
+                        // diminuir essa view para caber
+                        BlockView(block: puzzle.board.blocks[i]).onTapGesture {
+                            puzzle.selectBlock(block: puzzle.board.blocks[i])
+                        }
                     }
-                    
-                    // diminuir essa view para caber 
-                    BlockView(block: puzzle.board.blocks[i]).onTapGesture {
-                        puzzle.selectBlock(block: puzzle.board.blocks[i])
-                    }
-                }
-            }.padding(10)
+                }.padding(5)
+                
+            }.padding()
+            
         }.padding()
+        
     }
 }
 
