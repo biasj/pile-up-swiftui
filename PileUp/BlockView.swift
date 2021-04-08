c2df6bf0b6bdf93456d3e8d8837a575a7560466b
Beatriz Sato <50559620+biasj@users.noreply.github.com>
2021-04-08 16:06:53 -0300

    Adiciona drag no block

--- PileUp/BlockView.swift
+++ PileUp/BlockView.swift
@@ -7,8 +7,18 @@
 
 import SwiftUI
 
+enum DragState {
+    case unknown
+    case good
+    case bad
+}
+
+
 struct BlockView: View {
     var block: Block
+    
+    @State var dragAmount = CGSize.zero
+    @State var dragState = DragState.unknown
     
     var body: some View {
         ZStack {
@@ -20,6 +30,35 @@ struct BlockView: View {
             
             // adicionar pileView
             
+        }
+        .offset(dragAmount)
+        .zIndex(dragAmount == .zero ? 0 : 1)
+        .shadow(color: dragColor, radius: dragAmount == .zero ? 0 : 10)
+        // allows to drag the block aroung
+        .gesture(DragGesture(coordinateSpace: .global)
+                .onChanged {
+                    // as the drag around the screen happens, it updates it
+                    self.dragAmount = CGSize(width: $0.translation.width, height: $0.translation.height)
+                }
+             
+                // when the view is released
+                .onEnded { _ in
+                    self.dragAmount = .zero
+                }
+        )
+    }
+    
+    // define color based on DragState
+    var dragColor: Color {
+        switch dragState {
+        case .unknown:
+            return .black
+            
+        case .good:
+            return .green
+            
+        case .bad:
+            return .red
         }
     }
 }
--- PileUp/BoardView.swift
+++ PileUp/BoardView.swift
@@ -7,39 +7,39 @@
 
 import SwiftUI
 
+
 struct BoardView: View {
     
     @ObservedObject var puzzle : PuzzleGame
-    
-    private let columns: [GridItem] = Array(repeating: .init(.fixed(160), spacing: 10), count: 4)
+        
+    private let columns: [GridItem] = Array(repeating: .init(.fixed(160), spacing: 12), count: 4)
     
     var body: some View {
         ZStack {
             // grid background
-            RoundedRectangle(cornerRadius: 10).frame(width: 700, height: 450, alignment: .center).foregroundColor(.gray)
-            
-            
+//            RoundedRectangle(cornerRadius: 10).frame(width: 750, height: 450, alignment: .center).foregroundColor(.gray)
+
+//            LazyVGrid(columns: columns, spacing: 22) {
+//                ForEach(0..<puzzle.board.blocks.count) { i in
+//                    ZStack {
+//                        Image("shadow").resizable().frame(width: 150, height: 75, alignment: .center)
+//                        if puzzle.board.shouldPlaceGoal(index: i) {
+//                            GoalView(goal: puzzle.board.goals[puzzle.board.getGoalIndex(of: i)])
+//                        }
+//                    }
+//                }
+//
+//            }
+
             // board grid 4x4
-            LazyVGrid(columns: columns, spacing: 0) {
-                ForEach(0..<puzzle.board.blocks.count) { i in
-                    ZStack {
-                        Image("shadow").resizable().frame(width: 150, height: 75, alignment: .center)
-                        
-                        // check if any GoalView should be placed at the current index
-                        if puzzle.board.shouldPlaceGoal(index: i) {
-                            GoalView(goal: puzzle.board.goals[puzzle.board.getGoalIndex(of: i)])
-                        }
-                        
-                        // diminuir essa view para caber
-                        BlockView(block: puzzle.board.blocks[i]).onTapGesture {
-                            puzzle.selectBlock(block: puzzle.board.blocks[i])
-                        }
-                    }
-                }.padding(5)
-                
-            }.padding()
+            LazyVGrid(columns: columns, spacing: 22) {
+                    ForEach(0..<puzzle.board.blocks.count) { i in
+                            BlockView(block: puzzle.board.blocks[i])
+                    } // block padding
+            }
             
-        }.padding()
+            
+        }.padding() // whole board padding
         
     }
 }
